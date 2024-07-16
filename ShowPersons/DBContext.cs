bb1c2754ff5590ba90e63a26f13803d1304f424f
jshlomov <jonatanshv@gmail.com>
2024-07-16 10:11:28 +0300

    Add project files.

---
+++ ShowPersons/DBContext.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace ShowPersons
+{
+    public class DBContext
+    {
+        public string _connectionString;
+
+        public DBContext()
+        {
+
+            // SET SECRETS.JSON TO COPY ALWAYS AFTER ADDING!!
+            IConfiguration builder = new ConfigurationBuilder()
+                .AddJsonFile("secrets.json", optional: true) // Add secrets.json
+                .Build();
+            // Read a value from the configuration
+            _connectionString = builder["ConnectionString"];
+        }
+
+        public DataTable MakeQuery(string query)
+        {
+            DataTable output = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    try
+                    {
+                        connection.Open();
+                        using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                        {
+                            adapter.Fill(output);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                }
+            }
+            return output;
+        }
+
+        public DataTable MakeQuery(string queryStr, SqlParameter[] parameters)
+        {
+            DataTable output = new DataTable();
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(queryStr, conn))
+                {
+                    cmd.Parameters.AddRange(parameters);
+                    try
+                    {
+                        conn.Open();
+                        using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                        {
+                            adapter.Fill(output);
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        Console.WriteLine("An error occurred: " + ex.Message);
+                        // Further error handling
+                    }
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ ShowPersons/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShowPersons
+{
+    public partial class Form1 : Form
+    {
+        DBContext dbcontext;
+        public Form1()
+        {
+            InitializeComponent();
+            dbcontext = new DBContext();
+            string query = "select * from persons";
+            dataGridView1.DataSource = dbcontext.MakeQuery(query);
+        }
+    }
+}
